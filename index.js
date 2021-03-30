a5c5fc728fba8f613a4f5495c144cb8df9559ca0
Amanda <amanda@actualize.co>
2021-03-30 18:27:26 -0500

    frontend for chicago graffiti removal requests api data

---
+++ index.js
@@ -0,0 +1,72 @@
+/* global Chart, axios */
+
+let tally = [];
+let labels = [];
+for (let index = 0; index <= 50; index++) {
+  labels.push(`Ward ${index}`);
+}
+console.log(labels);
+
+axios.get("https://data.cityofchicago.org/resource/hec5-y4x5.json")
+  .then(function (response) {
+    // handle success
+    // console.log(response.data);
+    for (let removalRequest of response.data) {
+      let wardNumber = removalRequest.ward;
+      if (tally[wardNumber]) {
+        tally[wardNumber]++;
+      } else {
+        tally[wardNumber] = 1;
+      }
+    }
+    console.log(tally);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
+var ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: "# of Requests",
+        data: tally,
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        //   "rgba(255, 99, 132, 0.2)",
+        //   "rgba(54, 162, 235, 0.2)",
+        //   "rgba(255, 206, 86, 0.2)",
+        //   "rgba(75, 192, 192, 0.2)",
+        //   "rgba(153, 102, 255, 0.2)",
+        //   "rgba(255, 159, 64, 0.2)",
+        // ],
+        // borderColor: [
+        //   "rgba(255, 99, 132, 1)",
+        //   "rgba(54, 162, 235, 1)",
+        //   "rgba(255, 206, 86, 1)",
+        //   "rgba(75, 192, 192, 1)",
+        //   "rgba(153, 102, 255, 1)",
+        //   "rgba(255, 159, 64, 1)",
+        // ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});
